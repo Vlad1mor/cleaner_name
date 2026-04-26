b9235cf3d84afbe74c25b1fdb1102d7d7625152f
boyna <boyna1212@mail.ru>
2026-04-26 20:51:26 +0300

    Функция фильтрации строк через библиотеку re

---
+++ src/cleaner.py
@@ -0,0 +1,23 @@
+import re
+def keep_only_russian_letters(text: str) -> str:
+    """
+    Удаляет все строки, кроме русских букв
+    :param text: Вла,дими р
+    :return: Владимир
+    """
+    if not isinstance(text, str):
+        text = str(text)
+    return re.sub(r'[^а-яА-ЯёЁ]', '', text)
+def clean_dataframe(df, columns=None):
+    """
+    Применяет отчистку ко всем указанным колонка
+    Удаляет строки, если колонки пустые
+    """
+    if columns is None:
+        columns = df.select_dtypes(include=['object']).columns.tolist()
+    for col in columns:
+        df[col] = df[col].astype(str).apply(keep_only_russian_letters)
+
+    for col in columns:
+        df = df[df[col] != '']
+    return df
