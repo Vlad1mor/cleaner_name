dbc98053e29122b3df8214f30f10de2181b40b4b
boyna <boyna1212@mail.ru>
2026-04-29 20:47:11 +0300

    Тест для функции clean_dataframe

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -1,4 +1,6 @@
-from src.cleaner import keep_only_russian_letters
+import pandas as pd
+import pytest
+from src.cleaner import keep_only_russian_letters, clean_dataframe
 
 
 def test_keep_only_russian_letters_basic():
@@ -12,3 +14,15 @@ def test_keep_only_russian_letters_basic():
     assert keep_only_russian_letters("1223") == ""
     assert keep_only_russian_letters("avg") == ""
     assert keep_only_russian_letters(" ") == ""
+
+def test_clean_dataframe_removes_non_russian():
+    df = pd.DataFrame({
+        'name': ['Владимир', 'John', 'Петр', '123', ''],
+        'city': ['Москва', 'New York', 'СПБ', '!!!', '']
+    })
+    result = clean_dataframe(df, columns=['name', 'city'])
+    expected = pd.DataFrame({
+        'name': ['Владимир','Петр'],
+        'city': ['Москва', 'СПБ']
+    })
+    pd.testing.assert_frame_equal(result.reset_index(drop=True), expected)
